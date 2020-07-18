ef011ece0f123eb65f5045016791bb4c9def7323
nijisakai <hyinn@live.com>
2020-07-18 18:57:52 +0800

    update

--- mydemo/Program.cs
+++ mydemo/Program.cs
@@ -1,16 +1,40 @@
-﻿using System;
+﻿//命名空间
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//项目名称
 namespace mydemo
 {
+    //类名称，苦力
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World");
+            Console.ReadKey();
+            //Console.WriteLine("aloha! Hello, World");
+            //Console.ReadKey();
+        }
+        public static int GetMy(int n1, int n2)
+        {
+            return n1 > n2 ? n1 : n2;
+        }
 
+        public class Person
+        {
+            public int Age
+            {
+                get;
+                set;
+            }
+            public char Gender
+            {
+                get;
+                set;
+            }
         }
     }
 }
